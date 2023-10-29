4af3f9d89038020b67f830023d844152e99526c8
Noah Young <noahyoung15@gmail.com>
2023-10-29 01:32:04 -0400

    Initial Camera implementation.

--- app/src/main/java/com/example/itemclassifier/MainActivity.kt
+++ app/src/main/java/com/example/itemclassifier/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.example.itemclassifier
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.Manifest
+import android.app.Activity
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,8 +22,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Camera
 import androidx.compose.material.icons.filled.Menu
 //import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
@@ -31,31 +40,111 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.example.itemclassifier.data.Datasource
 import com.example.itemclassifier.model.Item
 import com.example.itemclassifier.ui.theme.ItemClassifierTheme
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
 
 class MainActivity : ComponentActivity() {
+    private lateinit var cameraExecutor: ExecutorService
+    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             ItemClassifierTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScaffold()
+                    MainScaffold(cameraRequest = ::cameraRequest)
+
+                    if (shouldShowCamera.value) {
+                        CameraView (
+                            executor = cameraExecutor
+                        )
+                    }
                 }
             }
         }
+    }
+
+    private val checkCameraPermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            shouldShowCamera.value = true
+        } else {
+            Toast.makeText(baseContext, "Camera access denied.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun cameraRequest() {
+        checkCameraPermission.launch(Manifest.permission.CAMERA)
+    }
+}
+
+@Composable
+fun CameraView (
+    executor: Executor
+) {
+    val lensFacing = CameraSelector.LENS_FACING_BACK
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    val preview = androidx.camera.core.Preview.Builder().build()
+    val previewView = remember {
+        PreviewView(context)
+    }
+    val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+    LaunchedEffect(lensFacing) {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+
+        cameraProviderFuture.addListener( {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            /* Preview
+            val preview = androidx.camera.core.Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                } */
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    lifecycleOwner,
+                    cameraSelector,
+                    preview)
+
+                preview.setSurfaceProvider(previewView.surfaceProvider)
+
+            } catch(exc: Exception) {
+                Log.e("TAG", "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(context))
     }
 }
 
@@ -102,8 +191,11 @@ private fun ItemCardPreview() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScaffold() {
+fun MainScaffold(
+    cameraRequest: () -> Unit
+) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    val activity = LocalContext.current as Activity
     Scaffold (
         topBar = {
             CenterAlignedTopAppBar(
@@ -138,8 +230,8 @@ fun MainScaffold() {
             )
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = {}) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
+            FloatingActionButton(onClick = {cameraRequest}) {
+                Icon(Icons.Default.Camera, contentDescription = "Add")
             }
         }
     ) { innerPadding ->
