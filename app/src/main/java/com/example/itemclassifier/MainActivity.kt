7bcee7758f0b504b6b540a9ec0490cb84fad6abd
Noah Young <noahyoung15@gmail.com>
2023-11-01 00:42:10 -0400

    Camera view open/close.

--- app/src/main/java/com/example/itemclassifier/MainActivity.kt
+++ app/src/main/java/com/example/itemclassifier/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -14,6 +15,7 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -24,6 +26,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Camera
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Menu
 //import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
@@ -44,6 +47,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -53,29 +57,34 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.itemclassifier.data.Datasource
 import com.example.itemclassifier.model.Item
 import com.example.itemclassifier.ui.theme.ItemClassifierTheme
 import java.util.concurrent.Executor
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
 
 class MainActivity : ComponentActivity() {
     private lateinit var cameraExecutor: ExecutorService
-    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             ItemClassifierTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScaffold(cameraRequest = ::cameraRequest)
-
+                    MainScaffold(requestCamera = ::requestCamera)
+                    Log.d("Test Value", "${shouldShowCamera.value}")
                     if (shouldShowCamera.value) {
                         CameraView (
                             executor = cameraExecutor
@@ -84,6 +93,28 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        //requestCamera()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun requestCamera() {
+        Log.d("TEST", "TEST CALL")
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                shouldShowCamera.value = true
+                Log.i("kilo", "Permission previously granted")
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CAMERA
+            ) -> Log.i("kilo", "Show camera permissions dialog")
+
+            else -> checkCameraPermission.launch(Manifest.permission.CAMERA)
+        }
     }
 
     private val checkCameraPermission = registerForActivityResult(
@@ -91,13 +122,10 @@ class MainActivity : ComponentActivity() {
     ) { isGranted ->
         if (isGranted) {
             shouldShowCamera.value = true
+            Log.d("Test Value", "${shouldShowCamera.value}")
         } else {
             Toast.makeText(baseContext, "Camera access denied.", Toast.LENGTH_SHORT).show()
         }
-    }
-
-    private fun cameraRequest() {
-        checkCameraPermission.launch(Manifest.permission.CAMERA)
     }
 }
 
@@ -121,13 +149,6 @@ fun CameraView (
         cameraProviderFuture.addListener( {
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            /* Preview
-            val preview = androidx.camera.core.Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
-                } */
-
             try {
                 // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
@@ -145,6 +166,17 @@ fun CameraView (
             }
 
         }, ContextCompat.getMainExecutor(context))
+    }
+
+    Box(contentAlignment = Alignment.TopEnd, modifier = Modifier.fillMaxSize()) {
+        AndroidView({previewView}, modifier = Modifier.fillMaxSize())
+
+        IconButton(onClick = { shouldShowCamera.value = false }) {
+            Icon(
+                imageVector = Icons.Filled.Close,
+                contentDescription = "Localized description"
+            )
+        }
     }
 }
 
@@ -192,10 +224,9 @@ private fun ItemCardPreview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScaffold(
-    cameraRequest: () -> Unit
+    requestCamera: () -> Unit
 ) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
-    val activity = LocalContext.current as Activity
     Scaffold (
         topBar = {
             CenterAlignedTopAppBar(
@@ -210,14 +241,6 @@ fun MainScaffold(
                         overflow = TextOverflow.Ellipsis
                     )
                 },
-                navigationIcon = {
-                    IconButton(onClick = { /* click action */ }) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                },
                 actions = {
                     IconButton(onClick = { /* click action */ }) {
                         Icon(
@@ -230,7 +253,7 @@ fun MainScaffold(
             )
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = {cameraRequest}) {
+            FloatingActionButton(onClick = { requestCamera() }) {
                 Icon(Icons.Default.Camera, contentDescription = "Add")
             }
         }
